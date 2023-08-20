c7517fc8669c8dcdd3753342fdcbc725683567fb
Christopher Moore <christopher.e.moore@gmail.com>
2023-08-20 21:14:16 +0700

    more

---
+++ obj/log.lua
@@ -0,0 +1,20 @@
+local vec3f = require 'vec-ffi.vec3f'
+local Obj = require 'zelda.obj.obj'
+
+local LogItem = Obj:subclass()
+
+LogItem.name = 'Log'
+LogItem.sprite = 'log'
+LogItem.useGravity = false
+LogItem.collidesWithTiles = false
+LogItem.min = vec3f(-.3, -.3, -.3)
+LogItem.max = vec3f(.3, .3, .3)
+
+function LogItem:touch(other)
+	if other.addItem then
+		other:addItem(LogItem)
+		self:remove()
+	end
+end
+
+return LogItem
