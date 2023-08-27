9022e08d167969594a50936353968e47c24f141f
Christopher Moore <christopher.e.moore@gmail.com>
2023-08-27 15:55:36 +0700

    added some mass image processing

---
+++ getsprites/process.lua
@@ -0,0 +1,107 @@
+#!/usr/bin/env luajit
+-- cycle through all sprites in jpeg
+-- histogram to find their greatest color
+-- replace it with alpha
+-- downsize too i.g. but what targt size?
+local ffi = require 'ffi'
+local path = require 'ext.path'
+local table = require 'ext.table'
+local Image = require 'image'
+
+local srcdir = path'srcsprites'
+local dstdir = path'dstsprites'
+dstdir:mkdir()
+for f in srcdir:dir() do
+	local src = srcdir/f
+	-- TODO hmm how about a :setext() function?
+	local dst = (dstdir/f):setext'png'
+	print('convert "'..src..'" "'..dst..'"')
+	--[[ imagemagick looks like it takes a lot of work to do this ...
+	-- https://stackoverflow.com/a/27194202/2714073
+	--]]
+	-- [[ here's another attempt
+	-- https://stackoverflow.com/a/73428638/2714073
+	-- looks good but you gotta know the background beforehand
+	--]]
+	-- [[ so I'll just use my own 
+	local srcimg = Image(tostring(src))
+	--print(require 'ext.tolua'(srcimg))
+	
+	local hist = srcimg:getHistogram()
+	local bgcount, bgcolor = table.sup(hist)
+	local br, bg, bb = bgcolor:byte(1,3)
+
+	local dstimg = srcimg
+		:setChannels(4)
+	
+	-- [=[
+	for j=0,dstimg.height-1 do
+		for i=0,dstimg.width-1 do
+			local p = dstimg.buffer + 4 * (i + dstimg.width * j)
+			local r,g,b = p[0], p[1], p[2]
+			-- TODO colorspace for measuring distance?
+			local dist = math.sqrt((br - r)^2 + (bg - g)^2 + (bb - b)^2)
+			if dist < 30 then
+				p[3] = 0
+			else
+				p[3] = 255
+			end
+		end
+	end
+	--]=]
+	
+-- [[ blob-detection because it looks like there's a lot of pixel blobs that could stand to be filtered out
+	local blobs = dstimg:getBlobs{
+		classify = function(p)
+			return p[3]			-- classify blobs by alpha
+		end,
+	}
+--]]
+
+	print('#blobs', #blobs)
+	--print(table(blobs):map(function(blob,k,t) return blob:calcArea(), #t+1 end):sort():concat', ')
+
+-- [=[
+	local tmp = Image(dstimg.width, dstimg.height, 4, 'unsigned char')
+		:clear()
+	-- [[
+	for _,blob in pairs(blobs) do
+		if blob.cl > 0 then
+			local area = blob:calcArea()
+			if area > 3000 then
+				blob:copyToImage(tmp, dstimg)
+			end
+		end
+	end
+	--]]
+	dstimg = tmp
+--]=]
+
+-- [=[
+	-- zealous-crop
+	dstimg = dstimg:zealousCrop()
+
+	local w, h = dstimg:size()
+	local s = math.max(w, h)
+	
+	--[[ recenter? 
+	dstimg = Image(s, s, dstimg.channels, dstimg.format):paste{
+		image = dstimg,
+		x = math.floor((s - w) / 2),
+		y = math.floor((s - h) / 2),
+	}
+	--]]
+
+
+	local targetsize = 64
+	if tostring(src):sub(1,4) == 'tree' then
+		targetsize = 128
+	end
+
+	dstimg = dstimg:resize(math.ceil(targetsize*w/s), math.ceil(targetsize*h/s))
+--]=]
+
+	dstimg:save(tostring(dst))
+	--]]
+	
+end
