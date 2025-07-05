b685f6cb0261b68a40eb2d72ab21f859cf631c26
thenumbernine <christopher.e.moore@gmail.com>
2025-07-05 19:49:58 +0800

    renamed it to match repo

--- genatlas/genatlas.sh
+++ genatlas/genatlas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # if texatlas.lua fails then don't overwrite:
 set -e
-../../texatlas/texatlas.lua "srcdir=sprites/" "borderTiled={'sprites/maptiles/'}"
+../../texture-atlas/texatlas.lua "srcdir=sprites/" "borderTiled={'sprites/maptiles/'}"
 mv atlas.lua ../sprites/
 mv atlas.png ../sprites/
